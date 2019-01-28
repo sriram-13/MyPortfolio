1573f285dc30470ce9d872100a1f703590762fd7
sriram <mail.sriram.13@gmail.com>
2019-01-28 14:30:02 +0530

    Experience model created

---
+++ mysite/myapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Experience, Responsibility
+
+# Register your models here.
+
+admin.site.register(Experience)
+admin.site.register(Responsibility)
---
+++ mysite/myapp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Experience(models.Model):
+    company = models.CharField(max_length = 30)
+    role = models.CharField(max_length = 25)
+    from_date = models.DateField()  
+    #Constraint should be added to from_date to not choose a future date
+    to_date = models.DateField()
+    #Constraint should be added to to_date to not choose a future date
+    current_company = models.BooleanField()
+
+    def __str__(self):
+        return (self.company, self.role)
+    
+class Responsibility(models.Model):
+    exp = models.ForeignKey(Experience, related_name='res_list', on_delete = models.CASCADE)
+    res = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return (self.res)
